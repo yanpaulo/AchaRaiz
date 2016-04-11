96d38c9dce244ff78535ed6d37814b6b38f83353
Yan Paulo <yanpaulo@hotmail.com>
2016-04-11 17:39:41 -0300

    Página web agora calcula as raízes.

--- Raiz.WebApp/Controllers/HomeController.cs
+++ Raiz.WebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NCalc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +14,28 @@ namespace Raiz.WebApp.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(string expression, double a = -1, double b = -1)
+        {
+            try
+            {
+                AchaRaiz ar = new AchaRaiz(new FuncaoTexto(expression));
+                double epsilon = 1e-5;
+                string saida = "" +
+                $"Bisseccao: {ar.Bisseccao(-3, -1, epsilon)}\n" +
+                $"MPF: {ar.MPF(-3, -1, epsilon, epsilon)}\n" +
+                $"MPF2: {ar.MPF2(-1.5, 0.1, 0.1)}\n" +
+                $"Raphson: {ar.Raphson(-3, epsilon, epsilon)}\n" +
+                $"Secante: {ar.Secante(-3, -1, epsilon, epsilon)}\n";
+                ViewBag.Saida = saida.Split(new string[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            catch (EvaluationException)
+            {
+                ModelState.AddModelError("expression", "Função inválida");
+            }
+            return View();
+
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
